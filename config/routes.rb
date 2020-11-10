98f6671baf8e7198421c109c95a85e0cac8b7a0b
EricCRodriguez <ecrod12@gmail.com>
2020-11-10 16:16:14 -0600

    updated frontend files ready to deploy

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
     get "/items/:id" => "items#show"
     patch "/items/:id" => "items#update"
     delete "/items/:id" => "items#destroy"
+
+    get "/*path" => proc { [200, {}, [ActionView::Base.new.render(file: "public/index.html")]] }
   end
 end
